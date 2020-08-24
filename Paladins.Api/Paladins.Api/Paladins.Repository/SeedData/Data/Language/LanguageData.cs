f16cd0a9276e6016312a1d669d90985f7c9e6877
5Gears0Chill <muaazjoosuf.git@gmail.com>
2020-08-24 20:07:27 +0200

    altered spacing for names

--- Paladins.Api/Paladins.Api/Paladins.Repository/SeedData/Data/Language/LanguageData.cs
+++ Paladins.Api/Paladins.Api/Paladins.Repository/SeedData/Data/Language/LanguageData.cs
@@ -6,7 +6,7 @@ namespace Paladins.Repository.SeedData.Data.Language
 {
     public class LanguageData : DataRoot
     {
-        private const string data = "{\"languages\":[{\"id\":1,\"name\":\"English\"},{\"id\":2,\"name\":\"German\"},{\"id\":3,\"name\":\"French\"},{\"id\":5,\"name\":\"Chinese\"},{\"id\":7,\"name\":\"Spanish\"},{\"id\":9,\"name\":\"Spanish(LatinAmerica)\"},{\"id\":10,\"name\":\"Portuguese\"},{\"id\":11,\"name\":\"Russian\"},{\"id\":12,\"name\":\"Polish\"},{\"id\":13,\"name\":\"Turkish\"}]}";
+        private const string data = "{\"languages\":[{\"id\":1,\"name\":\"English\"},{\"id\":2,\"name\":\"German\"},{\"id\":3,\"name\":\"French\"},{\"id\":5,\"name\":\"Chinese\"},{\"id\":7,\"name\":\"Spanish\"},{\"id\":9,\"name\":\"Spanish (Latin America)\"},{\"id\":10,\"name\":\"Portuguese\"},{\"id\":11,\"name\":\"Russian\"},{\"id\":12,\"name\":\"Polish\"},{\"id\":13,\"name\":\"Turkish\"}]}";
 
         public string GetData()
         {
